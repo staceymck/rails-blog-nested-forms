5fc928e7ab242576b967a9deb2b57185d09356c6
Stacey McKnight <mcknight.stm@gmail.com>
2021-02-22 13:09:58 -0600

    commit tree 852972289671846af1583d8b8ac2b476e9ebf4bd

--- app/models/post.rb
+++ app/models/post.rb
@@ -5,4 +5,16 @@ class Post < ActiveRecord::Base
   has_many :tags, :through => :post_tags
 
   validates_presence_of :name, :content
+
+  def tags_attributes=(tags_attributes)
+    tags_attributes.each do |i, tag_attribute|
+      if tag_attribute && tag_attribute[:name].present?
+        tag = Tag.find_or_create_by(tag_attribute)
+          if !self.tags.include?(tag) #don't add tag to post if already present in post.tags collection
+            self.post_tags.build(tag: tag) # why post_tags here?
+          end
+      end
+    end
+  end
+
 end
